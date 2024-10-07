2bc59260abd45783d22744fae19d6aae14101586
Lee Woei Chieh <leewoeichieh@yahoo.com.sg>
2024-10-07 08:43:58 +0800

    Edit title and prompt text in main.py for better clarity

--- capstone/streamlit/main.py
+++ capstone/streamlit/main.py
@@ -48,36 +48,36 @@ def main():
     """Main script to test out writing a Streamlit app."""
 
     # region <----------- Streamlit App Configuration -------------->
-    st.set_page_config(layout="centered", page_title="My Streamlit App")
+    st.set_page_config(layout="centered", page_title="Course Information")
     # endregion <----------- Streamlit App Configuration -------------->
 
     # Check if the user gave the correct password
     if not helper_functions.utility.check_password():
         st.stop()
 
-    st.title("Streamlit App")
+    st.title("AI Bootcamp Course Information")
 
     # Display disclaimer message
     helper_functions.disclaimer.display_disclaimer()
 
     form = st.form(key="form")
-    form.subheader("Prompt")
+    form.subheader("What do you want to know about AI Bootcamp?")
 
-    user_prompt = form.text_area("Enter your prompt here", height=200)
+    user_prompt = form.text_area("Example: Who can join AI Bootcamp?", height=200)
 
     if form.form_submit_button("Submit"):
         st.toast(f"User Input Submitted - {user_prompt}")
-        #response, course_details = logics.customer_query_handler.process_user_message(
+        # response, course_details = logics.customer_query_handler.process_user_message(
         #    user_prompt
-        #)
+        # )
         response = logics.customer_query_handler.rag_answer_query(user_prompt)
 
         st.write(response)
 
         st.divider()
 
-        #df_course_details = pd.json_normalize(course_details)
-        #st.dataframe(df_course_details)
+        # df_course_details = pd.json_normalize(course_details)
+        # st.dataframe(df_course_details)
 
         print(f'User Input is "{user_prompt}"')
 
